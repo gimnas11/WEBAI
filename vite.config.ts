d6cb46163644d64931672cd72e015b93b4b2b1ea
Gimnas Irwandi <gimnasirwandi@gmail.com>
2025-11-11 07:43:35 +0700

    Update config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WEBAI-GITHUB/',
+  base: '/WEBAI/',
   build: {
     outDir: 'dist',
     sourcemap: false,
